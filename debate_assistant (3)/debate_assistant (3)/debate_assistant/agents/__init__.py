e4451022b7482ca7c37f718128106ec414d3db31
jongtae1422-ux <jongtae1422@gmail.com>
2026-04-11 18:37:24 +0900

    Delete debate_assistant (3) directory

--- debate_assistant (3)/debate_assistant (3)/debate_assistant/agents/__init__.py
+++
@@ -1,4 +0,0 @@
-from .llm import call_ollama
-from .hint_agent import HintAgent
-from .summary_agent import SummaryAgent
-from .intro_agent import IntroAgent
--- debate_assistant (3)/debate_assistant (3)/debate_assistant/data/__init__.py
+++
@@ -1,1 +0,0 @@
-from .evidence import load_news_data_json, build_evidence_block, build_history_block, extract_last_ai_claim
